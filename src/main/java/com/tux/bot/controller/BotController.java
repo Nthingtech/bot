7411f3ccb8b08a73d379f374070235af30754f1d
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-11-07 22:16:34 -0300

    feat: Chat with model parameters

--- src/main/java/com/tux/bot/controller/BotController.java
+++ src/main/java/com/tux/bot/controller/BotController.java
@@ -2,6 +2,8 @@ package com.tux.bot.controller;
 
 import com.tux.bot.Question;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,21 @@ public class BotController {
        this.chatLanguageModel = chatLanguageModel;
    }
 
+   @Value("${OPENAI_KEY}")
+   private String apiKey;
+
     @PostMapping("/answer")
     public String aiBot(@RequestBody Question question) {
         return chatLanguageModel.generate(question.question());
+    }
+
+    @PostMapping("/answer2")
+    public String aiBot2(@RequestBody Question question) {
+       OpenAiChatModel customModel = OpenAiChatModel.builder() /*More generic --- ChatLanguageModel customModel = new OpenAiChatModel.OpenAiChatModelBuilder()*/
+               .apiKey(apiKey)
+               .temperature(0.1)
+               .build();
+       return customModel.generate(question.question());
     }
 
 }
